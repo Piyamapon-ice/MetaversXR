931b3f65497ca1e92c57b1c75c2f27e7603c11f1
Piyamapon-ice <85658553+Piyamapon-ice@users.noreply.github.com>
2023-01-03 17:24:03 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    runtime: 'experimental-edge',
-  },
+  // experimental: {
+  //   runtime: 'experimental-edge',
+  // },
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
 }
 
 module.exports = nextConfig
